918722b7260aa160f086e2a695ad477c1b607622
Gavin Morris <gavin.morris@health.telstra.com>
2019-04-29 13:27:42 +0800

    add ExpressionTest

---
+++ ExpressionTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTest
+{
+    class Patient
+    {
+        public string Surname { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"property name: {GetPropertyName(p => p.DateOfBirth)}");
+        }
+
+        static string GetPropertyName<TResult>(
+            Expression<Func<Patient, TResult>> propertySelector,
+            char delimiter = '.',
+            char endTrim = ')')
+        {
+            if (propertySelector.NodeType != ExpressionType.Lambda || propertySelector.Body.NodeType != ExpressionType.MemberAccess)
+                throw new ArgumentException("invalid property accessor");
+
+            return ((MemberExpression)propertySelector.Body).Member.Name;
+
+            // gives you: "o => o.Whatever"
+            var asString = propertySelector.ToString();
+
+            // make sure there is a beginning property indicator; the "." in "o.Whatever" -- this may not be necessary?
+            var firstDelim = asString.IndexOf(delimiter);
+
+            return firstDelim < 0
+                       ? asString
+                       : asString.Substring(firstDelim + 1).TrimEnd(endTrim);
+        }
+    }
+}
